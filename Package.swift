7a41ff08155600b0b31ac3a9c503fe3326b4eeeb
Peter Cordone <pcordone@gmail.com>
2020-05-10 22:12:50 -0400

    Added min support of iOS 13.0.

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "Accounting",
     platforms: [
         .macOS(.v10_15), // incorpoated Combine so that we can publish the ChartOfAccounts account list.
+        .iOS("13.0"), // incorpoated Combine so that we can publish the ChartOfAccounts account list.
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
